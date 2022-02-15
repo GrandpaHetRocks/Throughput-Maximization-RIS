8460849b17d8a2572904a29ebdd04b37f4419166
Ayush MS <ayushms0209@gmail.com>
2022-02-16 03:35:40 +0530

    --

--- monte_carlo_runs.m
+++ monte_carlo_runs.m
@@ -54,30 +54,32 @@ for runs=1:run
 
     grad_=[];
     final=[];
-    for i=1:k
-            sum3=0;
-            for j=1:k
-                if(j~=i)
-                    sum3=sum3+abs(hij_array(i,j)'+G_array(j)*theta'*Hri_array(:,j))^2*P(j);
-                end
-            end
-            gamma=abs(hij_array(i,i)'+G_array(i)*theta'*Hri_array(:,i))^2 * P(i)/sum3;
-            if(i==1)
-            sumrate1=[sumrate1 log2(1+gamma)];
-            end
-            if(i==2)
-            sumrate2=[sumrate2 log2(1+gamma)];
-            end
-            if(i==3)
-            sumrate3=[sumrate3 log2(1+gamma)];
-            end
-            if(i==4)
-            sumrate4=[sumrate4 log2(1+gamma)];
-            end
+%     for i=1:k
+%             sum3=0;
+%             for j=1:k
+%                 if(j~=i)
+%                     sum3=sum3+abs(hij_array(i,j)'+G_array(j)*theta'*Hri_array(:,j))^2*P(j);
+%                 end
+%             end
+%             gamma=abs(hij_array(i,i)'+G_array(i)*theta'*Hri_array(:,i))^2 * P(i)/sum3;
+%             if(i==1)
+%             sumrate1=[sumrate1 log2(1+gamma)];
+%             end
+%             if(i==2)
+%             sumrate2=[sumrate2 log2(1+gamma)];
+%             end
+%             if(i==3)
+%             sumrate3=[sumrate3 log2(1+gamma)];
+%             end
+%             if(i==4)
+%             sumrate4=[sumrate4 log2(1+gamma)];
+%             end
+% 
+%     end
 
-    end
 
-    net_sumrate=(sumrate1+sumrate2+sumrate3+sumrate4);
+%     net_sumrate=(sumrate1+sumrate2+sumrate3+sumrate4);
+    net_sumrate=sumrate_calc(hij_array,theta,Hri_array,G_array,k,P,sumrate1,sumrate2,sumrate3,sumrate4);
     final=[final net_sumrate];
 
         for iterates=1:50
